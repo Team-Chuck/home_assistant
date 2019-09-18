e511a86b972b3b48d6c57b5f94a246e299911b40
Arsenii Astashkin <ars.astashkin@gmail.com>
2019-09-18 09:42:03 -0700

    customization + readme update

---
+++ README.md
@@ -0,0 +1,15 @@
+## Office Assistant Extension
+
+The hackathon project for facility assistant.
+
+Start the server:
+```
+python -m office_assistant build
+python -m office_assistant run --port=<PORT>
+```
+
+Debug the conversation flow locally:
+```
+python -m office_assistant build
+python -m office_assistant converse
+```
--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """This module contains the MindMeld home assistant blueprint application"""
-from home_assistant.root import app
+from office_assistant.root import app
 
-import home_assistant.greeting
-import home_assistant.smart_home
-import home_assistant.times_and_dates
-import home_assistant.unknown
-import home_assistant.weather  # noqa: ignore=F401
-import home_assistant.easter_egg
+import office_assistant.greeting
+import office_assistant.smart_home
+import office_assistant.times_and_dates
+import office_assistant.unknown
+import office_assistant.weather  # noqa: ignore=F401
+import office_assistant.easter_egg
 
 __all__ = ['app']
